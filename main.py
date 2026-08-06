cad15635ae97c00d8bfb5d8197671bdaa7188116
Sachinshettys90 <sachinshettys90@gmail.com>
2026-08-06 21:46:17 +0530

    Updated API code for Patient management system

--- main.py
+++ main.py
@@ -1,18 +1,28 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()  # defining the object
 
 
+def load_data():
+    with open('patient.json', 'r', encoding='utf-8') as f:
+        data = json.load(f)
+
+    return data
+
+
 @app.get("/")  # using decorator routing the / , with get method
 def hello():
-    return {"message": "hello world"}
+    return {"message": "Patient Management system API"}
 
 
 @app.get('/about')
 def about():
-    return {"message": "Campusx is an education platform where you can learn api "}
+    return {"message": "A fully functional API to manage your patient records"}
 
 
-@app.get('/info')
-def info():
-    return {"message": "this is the url info will be updated in the application"}
+@app.get('/view')
+def view():
+    data = load_data()
+
+    return data
