c1141e13ede89ee55335e08207f688123e71eeba
Sachinshettys90 <sachinshettys90@gmail.com>
2026-08-09 20:35:50 +0530

    Updated Sortpatient Query parameters

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Query
 import json
 
 app = FastAPI()  # defining the object
@@ -38,3 +38,27 @@ def view_patient(patient_id: str = Path(..., description="ID of the patient in t
         return data[patient_id]
     # HTTP Exceptions
     raise HTTPException(status_code=404, detail="Patient not found")
+
+
+# Queryparameters
+# /patients?city=Delhi&sort_by=age
+
+@app.get("/sort")
+def sort_patients(sort_by: str = Query(..., description="sort on the basis of height, weight or bmi"), order: str = Query('asc', description="sort in asc or desc order")):
+    valid_fields = ['height', 'weight', 'bmi']
+
+    if sort_by not in valid_fields:
+        raise HTTPException(
+            status_code=400, detail=f'Invalid field select from {valid_fields}')
+
+    if order not in ['asc', 'desc']:
+        raise HTTPException(
+            status_code=400, detail=f'Invalid order select betweem asc and desc')
+
+    data = load_data()
+    sort_order = True if order == 'desc' else False
+
+    sorted_data = sorted(data.values(), key=lambda x: x.get(
+        sort_by, 0), reverse=sort_order)
+
+    return sorted_data
