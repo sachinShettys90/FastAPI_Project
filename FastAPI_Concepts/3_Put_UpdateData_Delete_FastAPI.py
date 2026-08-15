3c4f879b2f38f2958b9b109b6d52e7676477b6ae
Sachinshettys90 <sachinshettyss85@gmail.com>
2026-08-15 21:38:20 +0530

    Put-Delete method--Deleted exiting fields

--- FastAPI_Concepts/3_Put_UpdateData_Delete_FastAPI.py
+++ FastAPI_Concepts/3_Put_UpdateData_Delete_FastAPI.py
@@ -94,3 +94,17 @@ def update_patient(patient_id: str, patient_update: Patient_Update):
     save_data(data)
 
     return JSONResponse(status_code=200, content={'message': 'Patient updated successfully'})
+
+
+@app.delete('/delete/{patient_id}')
+def delete_patient(patient_id: str):
+    data = load_data()
+
+    if patient_id not in data:
+        raise HTTPException(status_code=404, detail='Patient not found')
+
+    del data[patient_id]
+
+    save_data(data)
+
+    return JSONResponse(status_code=200, content={'message': 'Patient deleted successfully'})
